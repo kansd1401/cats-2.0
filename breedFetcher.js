6055bb1478d6a0437d31fd4dc5d22798ddd16a30
kansd1401 <kang.sanjeet1401@gmail.com>
2019-10-24 21:59:04 +0000

    linted bois

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,16 +5,15 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   const link = 'https://api.thecatpi.com/v1/breeds/search?q=' + breedName;
   request(link, (error, response, body) => {
-    if(error){
-      callback('Api down')
-    }
-    else if (JSON.parse(body).length !== 0) {
-     callback(JSON.parse(body)[0]['description']);
+    if (error) {
+      callback('Api down');
+    } else if (JSON.parse(body).length !== 0) {
+      callback(JSON.parse(body)[0]['description']);
     } else {
-     callback('Please enter a valid breed name');
+      callback('Please enter a valid breed name');
     }
   });
 };
 
-module.exports = { fetchBreedDescription }
+module.exports = { fetchBreedDescription };
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {fetchBreedDescription} = require('./breedFetcher')
-const breed = process.argv[2]
+const {fetchBreedDescription} = require('./breedFetcher');
+const breed = process.argv[2];
 
 fetchBreedDescription(breed,(data) => console.log(data));

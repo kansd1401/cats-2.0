11fba2bfe4226afba433f56aadedfb57de9f0485
kansd1401 <kang.sanjeet1401@gmail.com>
2019-10-24 21:20:53 +0000

    working boi

---
+++ breedFetcher.js
@@ -0,0 +1,12 @@
+const request = require('request');
+const link = 'https://api.thecatapi.com/v1/breeds/search?q=' + process.argv[2];
+
+request(link, (error, response, body) => {
+  const data = JSON.parse(body);
+  if (data.length !== 0) {
+    console.log('statusCode:', response && response.statusCode);
+    console.log(data[0]['description']);
+  } else {
+    console.log('Please enter a valid breed name');
+  }
+});
